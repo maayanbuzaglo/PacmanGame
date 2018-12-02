490e65012a786ab5ba26be6f6eb47a221fbf2618
מעיין בוזגלו <44781616+maayanbuzaglo@users.noreply.github.com>
2018-12-02 16:32:39 +0200

    Update Main.java

--- src/File_format/Main.java
+++ src/File_format/Main.java
@@ -2,14 +2,14 @@ package File_format;
 
 import java.util.ArrayList;
 
-public class Main {
+public class Example {
 	public static void main(String[] args) {
 
 		Csv2kml r = new Csv2kml();
-		ArrayList<Row_Locate> Csv = r.ReadCsvFile("C:\\Users\\îòééï\\Desktop\\Ex2\\data\\WigleWifi_20171201110209.csv");
+		ArrayList<Row_Locate> Csv = r.ReadCsvFile("C:\\Users\\Ã®Ã²Ã©Ã©Ã¯\\Desktop\\Ex2\\data\\WigleWifi_20171201110209.csv");
 		for (Row_Locate it: Csv) {
 			System.out.println(it.toString());
 		}
-		r.createKML(Csv,"C:\\Users\\îòééï\\Desktop\\Ex2\\data\\f.kml");
+		r.createKML(Csv,"C:\\Users\\Ã®Ã²Ã©Ã©Ã¯\\Desktop\\Ex2\\data\\f.kml");
 	}
 }
